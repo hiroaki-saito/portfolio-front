f7cfc976987bcc70ed50590804aee54d02c4196e
saito-hiroaki <hiro021312@gmail.com>
2021-01-03 18:13:27 +0900

    [add]AppBarDense

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import DenseAppBar from "./views/components/AppBarDense";
+
+function App() {
+  return (
+    <div className="App">
+      <DenseAppBar></DenseAppBar>
+    </div>
+  );
+}
+
+export default App;
